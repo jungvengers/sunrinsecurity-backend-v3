b27d30e1f67c71ded7c5c29b714eedf94af4b4dc
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-11-20 16:07:26 +0900

    REFACTOR index.ts
    for seperating connectDB, app

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,13 @@ import dotenv from "dotenv"
 dotenv.config()
 
 import express from "express"
-import morgan from "morgan"
-import bodyParser from "body-parser"
-import passport from "passport"
-import swaggerJSDoc from "swagger-jsdoc"
 import swaggerUI from "swagger-ui-express"
-import swaggerOptions from "config/swagger"
-import passportConfig from "config/passport"
 import { ErrorType, errorMessages } from "errors"
-import routes from "routes"
+import connectDB from "config/connectDB"
+connectDB()
 
-require("config/connectDB")
-//DB
+import app from "app"
 
-const app = express()
-const swaggerSpec = swaggerJSDoc(swaggerOptions)
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec))
 app.get("/custom-errors", (req: express.Request, res: express.Response) => {
     res.json({
@@ -24,10 +16,5 @@ app.get("/custom-errors", (req: express.Request, res: express.Response) => {
         errorMessages: errorMessages,
     })
 })
-app.use(bodyParser.json())
-app.use(morgan("combined"))
-app.use(passport.initialize())
-passportConfig()
 
-app.use("", routes)
 app.listen(process.env.PORT || 3000)
