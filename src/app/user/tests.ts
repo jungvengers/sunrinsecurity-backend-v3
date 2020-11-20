eb22ca30707d1b849613e4d161d47ed14870e9be
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-11-20 16:18:40 +0900

    REFACTOR app/user/tests.ts
    To use supertest instead of httpMocks,
    for testing the middlewares and controllers at the same time

--- src/app/user/tests.ts
+++ src/app/user/tests.ts
@@ -1,41 +1,83 @@
 import assert from "assert"
-import httpMocks from "node-mocks-http"
-import { register, createToken } from "app/user/controllers"
+import request from "supertest"
+import mongoose from "mongoose"
+import app from "app"
 import { User } from "app/user/models"
+import connectDB from "config/connectDB"
+
+const username = "testaccount"
+const password = "testpassword"
 
 describe("User", () => {
-    before((done) => {
-        require("config/connectDB")
-        User.deleteMany({}, done)
+    before(async () => {
+        connectDB()
+        await User.deleteMany({}, () => {})
     })
 
     describe("Registration", function () {
-        it("Success Case 1", async function () {
-            const req = httpMocks.createRequest({
-                method: "POST",
-                url: "/user/register",
-                body: {
-                    username: "testaccount",
-                    password: "testpassword1234",
-                },
+        describe("Success Case", async function () {
+            it("Proper Information", async function () {
+                await request(app)
+                    .post("/user/register")
+                    .send({
+                        username: username,
+                        password: password,
+                        alias: "thetestaccount",
+                    })
+                    .expect(201)
             })
-            const res = httpMocks.createResponse()
-            await register(req, res)
         })
-        it("Failure case 1", async () => {
-            const req = httpMocks.createRequest({
-                method: "POST",
-                url: "/user/register",
-                body: {
-                    username: "testaccount",
-                    password: "testpassword1234",
-                },
-            })
-            const res = httpMocks.createResponse()
 
-            await register(req, res)
-            const statusCode = res._getStatusCode()
-            assert.strictEqual(statusCode, 400)
+        describe("Failure Case", async function () {
+            it("Duplicate User", async () => {
+                await request(app)
+                    .post("/user/register")
+                    .send({
+                        username: "testaccount",
+                        password: "testpassword1234",
+                        alias: "thetestaccount",
+                    })
+                    .expect(400)
+            })
         })
+    })
+
+    describe("Authentication", function () {
+        describe("Success Case", async function () {
+            it("Proper Information", async function () {
+                const res = await request(app).post("/user/auth/token").send({
+                    username: username,
+                    password: password,
+                })
+                const data = JSON.parse(res.text)
+                assert.strictEqual(username, data.user.username)
+            })
+        })
+
+        describe("Failure Case", async function () {
+            it("Wrong Username", async function () {
+                await request(app)
+                    .post("/user/auth/token")
+                    .send({
+                        username: "wrongusername",
+                        password: "wrongpassword",
+                    })
+                    .expect(400)
+            })
+            it("Wrong Password", async function () {
+                await request(app)
+                    .post("/user/auth/token")
+                    .send({
+                        username: username,
+                        password: "wrongpassword",
+                    })
+                    .expect(400)
+            })
+        })
+    })
+
+    after(async () => {
+        await User.deleteMany({}, () => {})
+        await mongoose.disconnect()
     })
 })
