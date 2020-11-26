476be0ee34ea1d465caf9980b77e1e8238f7274b
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-11-27 03:34:22 +0900

    UPDATE to add addArticle function in app/user/controllers.ts

--- src/app/article/controllers.ts
+++ src/app/article/controllers.ts
@@ -1,7 +1,45 @@
 import { Request, Response } from "express"
+import { Article, Club, Kind } from "app/article/models"
+import { UserModel } from "app/user/models"
+import { isValidObjectId } from "mongoose"
 
-const getArticles = (req: Request, res: Response) => {
-    // filter articles by category, club, kind
+const addArticle = async (req: Request, res: Response) => {
+    const currentUser: UserModel | any = req.user
+    const { isContestWork, participants, clubs, content, kinds } = req.body
+    const clubsEnum: Club[] = []
+    const kindsEnum: Kind[] = []
+    Array.isArray(clubs)
+        ? clubs.forEach((club) => {
+              if (club in Club) {
+                  clubsEnum.push(club)
+              }
+          })
+        : null
+    Array.isArray(kinds)
+        ? kinds.forEach((kind) => {
+              if (kind in Kind) {
+                  kindsEnum.push(kind)
+              }
+          })
+        : null
+
+    const articleDocument = {
+        writer: currentUser.username,
+        isContestWork: isContestWork,
+        participants: participants,
+        clubs: clubsEnum,
+        content: content,
+        kinds: kindsEnum,
+    }
+
+    try {
+        const article = await Article.create(articleDocument)
+        return res.status(201).send(article)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send()
+    }
+}
 }
 
 const addArticle = (req: Request, res: Response) => {
