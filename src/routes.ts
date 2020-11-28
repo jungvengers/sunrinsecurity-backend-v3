ecae9b5e85b9d9a901376dfb75ea10c76e41de7a
YeonGyu Kim <code.yeon.gyu@gmail.com>
2020-11-28 18:08:31 +0900

    UPDATE to add mediaRouter in app/routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import userRouter from "app/user/routes"
 import articleRouter from "app/article/routes"
+import mediaRouter from "app/media/routes"
 import { Router } from "express"
 
 const router = Router()
 
 router.use("/user", userRouter)
 router.use("/article", articleRouter)
+router.use("/media", mediaRouter)
 
 export default router
