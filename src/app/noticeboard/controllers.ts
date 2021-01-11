98d3257d2f7a402875346e66b9b244a010566e8f
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-01-11 21:56:49 +0900

    ADD app/noticeboard/controllers.ts for noticeboard feature

---
+++ src/app/noticeboard/controllers.ts
@@ -0,0 +1,107 @@
+import { Request, Response } from "express"
+import { Notice } from "app/noticeboard/models"
+import { UserModel } from "app/user/models"
+import { isValidObjectId } from "mongoose"
+
+const addNotice = async (req: Request, res: Response) => {
+    const currentUser: UserModel | any = req.user
+    const { title, content } = req.body
+
+    const noticeDocument = {
+        writer: currentUser.username,
+        title: title,
+        content: content,
+    }
+
+    try {
+        const notice = await Notice.create(noticeDocument)
+        return res.status(201).send(notice)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send()
+    }
+}
+
+const getNotice = async (req: Request, res: Response) => {
+    const noticeID = req.params["id"]
+    if (!isValidObjectId(noticeID)) {
+        return res.status(404).send()
+    }
+    const isNoticeExists = await Notice.exists({ _id: noticeID })
+    if (!isNoticeExists) {
+        return res.status(404).send()
+    }
+    const notice = await Notice.findById(req.params["id"])
+    return res.send(notice)
+}
+
+const updateNotice = async (req: Request, res: Response) => {
+    const currentUser: UserModel | any = req.user
+
+    const noticeID = req.params["id"]
+    if (!isValidObjectId(noticeID)) {
+        return res.status(404).send()
+    }
+    const isNoticeExists = await Notice.exists({ _id: noticeID })
+    if (!isNoticeExists) {
+        return res.status(404).send()
+    }
+
+    let notice = await Notice.findById(noticeID)
+    if (notice?.writer !== currentUser.username) {
+        return res.status(403).send()
+    }
+
+    for (let key in req.body) {
+        if (notice?.get(key) && notice?.get(key) !== req.body[key]) {
+            notice.set(key, req.body[key])
+        }
+    }
+
+    await notice?.save()
+
+    return res.send(notice)
+}
+
+const deleteNotice = async (req: Request, res: Response) => {
+    const currentUser: UserModel | any = req.user
+
+    const noticeID = req.params["id"]
+    if (!isValidObjectId(noticeID)) {
+        return res.status(404).send()
+    }
+    const isNoticeExists = await Notice.exists({ _id: noticeID })
+    if (!isNoticeExists) {
+        return res.status(404).send()
+    }
+
+    const notice = await Notice.findById(noticeID)
+    if (notice?.writer !== currentUser.username) {
+        return res.status(403).send()
+    }
+    await Notice.findOneAndDelete({ _id: noticeID })
+
+    return res.status(200).send()
+}
+
+const getNotices = async (req: Request, res: Response) => {
+    let page: number = (req.query.page ? +(req.query.page as string) : 1) - 1
+    page = page < 0 ? 0 : page
+    let per_page: number | undefined = req.query.per_page
+        ? +(req.query.per_page as string)
+        : 15
+    per_page = per_page < 0 ? 0 : per_page
+
+    const notices = await Notice.find({})
+        .skip(page * per_page)
+        .limit(per_page)
+        .sort({ _id: -1 })
+
+    const noticesCount = await Notice.countDocuments({})
+    const pages = Math.ceil(noticesCount / per_page)
+    // filtering feature here
+
+    return res.send({ notices: notices, pages_length: pages })
+}
+
+export { addNotice, getNotice, updateNotice, deleteNotice, getNotices }
