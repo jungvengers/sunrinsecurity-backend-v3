16f1c7b55d3764f90b8417cd6041919a142b58e3
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2023-02-17 18:00:16 +0900

    fix: add typeorm import to application

--- packages/server/src/application/application.module.ts
+++ packages/server/src/application/application.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ApplicationService } from './application.service';
 import { ApplicationController } from './application.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Application } from './entities/application.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Application])],
   controllers: [ApplicationController],
-  providers: [ApplicationService]
+  providers: [ApplicationService],
 })
 export class ApplicationModule {}
