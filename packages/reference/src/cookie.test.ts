2387a2e13d18684202cbfdb8cc4f3e16318a324c
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2023-02-19 00:07:10 +0900

    test: add coverage cookie

--- packages/reference/src/cookie.test.ts
+++ packages/reference/src/cookie.test.ts
@@ -1,8 +1,7 @@
 import { REFRESH_TOKEN_KEY, REFRESH_TOKEN_OPTION } from './cookie';
+import { isProduction } from './util';
 
-jest.mock('./util', () => ({
-  isProduction: () => true,
-}));
+jest.mock('./util');
 
 describe('Cookie', () => {
   beforeEach(() => {
@@ -12,13 +11,24 @@ describe('Cookie', () => {
 
   it('should be defined', () => {
     expect(REFRESH_TOKEN_KEY).toBeDefined();
-    expect(REFRESH_TOKEN_KEY).toEqual('Refresh');
-
     expect(REFRESH_TOKEN_OPTION).toBeDefined();
+  });
+
+  it('should be truthy', () => {
+    (isProduction as jest.Mock).mockReturnValue(true);
     expect(REFRESH_TOKEN_OPTION()).toEqual({
       domain: 'test.com',
       httpOnly: true,
       secure: true,
+      maxAge: 2592000000,
+    });
+  });
+
+  it('should be falsy', () => {
+    (isProduction as jest.Mock).mockReturnValue(false);
+    expect(REFRESH_TOKEN_OPTION()).toEqual({
+      httpOnly: false,
+      secure: false,
       maxAge: 2592000000,
     });
   });
