778eb221fe5963b02b3a5a56ef5159a7d2619104
rmagur1203 <rmagur1203@naver.com>
2023-02-24 02:06:20 +0900

    test: add cache module of root test module (#5)

--- packages/server/src/__tests__/root.module.ts
+++ packages/server/src/__tests__/root.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Admin } from 'src/admin/entities/admin.entity';
@@ -7,12 +7,18 @@ import { Club } from 'src/club/entities/club.entity';
 import { Form } from 'src/form/entities/form.entity';
 import { Notice } from 'src/notice/entities/notice.entity';
 import { Project } from 'src/project/entities/project.entity';
+import ms from 'ms';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [`../../.env`, `.env`],
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      ttl: ms('5s'),
+      max: 100,
     }),
     TypeOrmModule.forRootAsync({
       useFactory: async () => ({
