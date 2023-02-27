b4eefebb0b5c7f92195ef0f476c1cfc204132df4
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2023-02-28 01:04:59 +0900

    feat: create form with club create

--- packages/server/src/club/club.module.ts
+++ packages/server/src/club/club.module.ts
@@ -3,9 +3,10 @@ import { ClubService } from './club.service';
 import { ClubController } from './club.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Club } from './entities/club.entity';
+import { Form } from 'src/form/entities/form.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Club])],
+  imports: [TypeOrmModule.forFeature([Club, Form])],
   controllers: [ClubController],
   providers: [ClubService],
 })
