19b5a061d3143905f69c3befcee722174a9bc965
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2023-02-28 01:19:54 +0900

    test: fix error

--- packages/server/src/club/club.service.spec.ts
+++ packages/server/src/club/club.service.spec.ts
@@ -3,13 +3,14 @@ import { ClubService } from './club.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Club } from './entities/club.entity';
 import { RootTestModule } from 'src/__tests__/root.module';
+import { Form } from 'src/form/entities/form.entity';
 
 describe('ClubService', () => {
   let service: ClubService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [RootTestModule, TypeOrmModule.forFeature([Club])],
+      imports: [RootTestModule, TypeOrmModule.forFeature([Club, Form])],
       providers: [ClubService],
     }).compile();
 
