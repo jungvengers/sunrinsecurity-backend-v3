eaf27b4b18bbc6f06bf97e0d58f7eb2c39253ebc
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2023-02-28 01:15:30 +0900

    test: fix error

--- packages/server/src/club/club.controller.spec.ts
+++ packages/server/src/club/club.controller.spec.ts
@@ -4,6 +4,7 @@ import { ClubService } from './club.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Club } from './entities/club.entity';
 import { RootTestModule } from 'src/__tests__/root.module';
+import { Form } from 'src/form/entities/form.entity';
 
 jest.mock('typeorm');
 
@@ -12,7 +13,7 @@ describe('ClubController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [RootTestModule, TypeOrmModule.forFeature([Club])],
+      imports: [RootTestModule, TypeOrmModule.forFeature([Club, Form])],
       controllers: [ClubController],
       providers: [ClubService],
     }).compile();
