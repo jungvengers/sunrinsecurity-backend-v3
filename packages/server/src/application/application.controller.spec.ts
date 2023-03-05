aace7db40ccb0cf9e850cbe54d4835901160836a
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2023-03-05 23:46:35 +0900

    test: fix test

--- packages/server/src/application/application.controller.spec.ts
+++ packages/server/src/application/application.controller.spec.ts
@@ -4,13 +4,14 @@ import { ApplicationService } from './application.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Application } from './entities/application.entity';
 import { RootTestModule } from 'src/__tests__/root.module';
+import { Club } from 'src/club/entities/club.entity';
 
 describe('ApplicationController', () => {
   let controller: ApplicationController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [RootTestModule, TypeOrmModule.forFeature([Application])],
+      imports: [RootTestModule, TypeOrmModule.forFeature([Application, Club])],
       controllers: [ApplicationController],
       providers: [ApplicationService],
     }).compile();
