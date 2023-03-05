36b30f3c0f83991a285c799f4d906cf78c44e0f9
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2023-03-05 20:56:17 +0900

    fix: add nullable to answer

--- packages/server/src/application/entities/application.entity.ts
+++ packages/server/src/application/entities/application.entity.ts
@@ -22,34 +22,34 @@ export class Application {
   // @Column()
   // portfolio!: string;
 
-  @Column('text')
+  @Column('text', { nullable: true })
   answer1!: string;
 
-  @Column('text')
+  @Column('text', { nullable: true })
   answer2!: string;
 
-  @Column('text')
+  @Column('text', { nullable: true })
   answer3!: string;
 
-  @Column('text')
+  @Column('text', { nullable: true })
   answer4!: string;
 
-  @Column('text')
+  @Column('text', { nullable: true })
   answer5!: string;
 
-  @Column('text')
+  @Column('text', { nullable: true })
   answer6!: string;
 
-  @Column('text')
+  @Column('text', { nullable: true })
   answer7!: string;
 
-  @Column('text')
+  @Column('text', { nullable: true })
   answer8!: string;
 
-  @Column('text')
+  @Column('text', { nullable: true })
   answer9!: string;
 
-  @Column('text')
+  @Column('text', { nullable: true })
   answer10!: string;
 
   @ManyToOne(() => Club, (club) => club.applications)
