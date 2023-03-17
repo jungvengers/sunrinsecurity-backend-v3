127a5bac4d07212fba350805ac7be26a456a4d53
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2023-03-17 21:59:49 +0900

    feat add validator of /form

--- packages/server/src/form/dto/create-form.dto.ts
+++ packages/server/src/form/dto/create-form.dto.ts
@@ -1,48 +1,59 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber } from 'class-validator';
+import { IsInt, IsOptional, IsString } from 'class-validator';
 
 export class CreateFormDto {
   @ApiProperty({ example: 1, description: '동아리 ID' })
-  @IsNumber(
-    {
-      allowNaN: false,
-      allowInfinity: false,
-      maxDecimalPlaces: 0,
-    },
-    {
-      message: '동아리 ID는 숫자로 입력해주세요.',
-    },
-  )
+  @IsInt({ message: '동아리 ID는 정수로 입력해주세요.' })
   clubid!: number;
 
   @ApiProperty({ example: '질문1', description: '질문' })
+  @IsOptional()
+  @IsString({ message: '질문은 문자열로 입력해주세요.' })
   question1: string;
 
   @ApiProperty({ example: '질문2', description: '질문' })
+  @IsOptional()
+  @IsString({ message: '질문은 문자열로 입력해주세요.' })
   question2: string;
 
   @ApiProperty({ example: '질문3', description: '질문' })
+  @IsOptional()
+  @IsString({ message: '질문은 문자열로 입력해주세요.' })
   question3: string;
 
   @ApiProperty({ example: '질문4', description: '질문' })
+  @IsOptional()
+  @IsString({ message: '질문은 문자열로 입력해주세요.' })
   question4: string;
 
   @ApiProperty({ example: '질문5', description: '질문' })
+  @IsOptional()
+  @IsString({ message: '질문은 문자열로 입력해주세요.' })
   question5: string;
 
   @ApiProperty({ example: '질문6', description: '질문' })
+  @IsOptional()
+  @IsString({ message: '질문은 문자열로 입력해주세요.' })
   question6: string;
 
   @ApiProperty({ example: '질문7', description: '질문' })
+  @IsOptional()
+  @IsString({ message: '질문은 문자열로 입력해주세요.' })
   question7: string;
 
   @ApiProperty({ example: '질문8', description: '질문' })
+  @IsOptional()
+  @IsString({ message: '질문은 문자열로 입력해주세요.' })
   question8: string;
 
   @ApiProperty({ example: '질문9', description: '질문' })
+  @IsOptional()
+  @IsString({ message: '질문은 문자열로 입력해주세요.' })
   question9: string;
 
   @ApiProperty({ example: '질문10', description: '질문' })
+  @IsOptional()
+  @IsString({ message: '질문은 문자열로 입력해주세요.' })
   question10: string;
 
   constructor(
--- packages/server/src/form/dto/update-form.dto.ts
+++ packages/server/src/form/dto/update-form.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateFormDto } from './create-form.dto';
 
-export class UpdateFormDto extends PartialType(CreateFormDto) {}
+export class UpdateFormDto extends OmitType(CreateFormDto, ['clubid']) {}
