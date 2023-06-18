32d02e16b03daf537fc85736f87d04420f689860
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2023-06-18 15:03:52 +0900

    change: project compare to include

--- packages/server/src/utils/compare.string.ts
+++ packages/server/src/utils/compare.string.ts
@@ -4,3 +4,10 @@ export function compare(str1: string | undefined, str2: string | undefined) {
   }
   return str1.localeCompare(str2, undefined, { sensitivity: 'accent' }) === 0;
 }
+
+export function include(str1: string | undefined, str2: string | undefined) {
+  if (!str1 || !str2) {
+    return false;
+  }
+  return str1.toLowerCase().includes(str2.toLowerCase());
+}
